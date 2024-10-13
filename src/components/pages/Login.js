df6348891f6ebadcf8580152cfcb8717555774ef
mingjun.geng <mingjun.geng@gmail.com>
2024-10-13 16:47:18 -0700

    improve components - Login with form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,39 @@
+import React, { useState } from 'react';
+
 export const Login = () => {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+
+    }
+
+    const handleLogin = (e) => {
+
+    }
+
     return (
         <div>
-            <p> LoginPage</p>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="text"
+                    name="username"
+                    value={credentials.username}
+                    onChange={handleChange}
+                    placeholder="Username"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={credentials.password}
+                    onChange={handleChange}
+                    placeholder="Password"
+                />
+                <button type="submit">Log in</button>
+            </form>
+
         </div>
     )
 }
