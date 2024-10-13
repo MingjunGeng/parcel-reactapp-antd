e4576829780bedbd41f87967159d5ed092371fc0
mingjun.geng <mingjun.geng@gmail.com>
2024-10-13 16:59:43 -0700

    improve components - Login with credentials

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+import "../../../styles/Login.less"
 
 export const Login = () => {
     const [credentials, setCredentials] = useState({
         username: '',
         password: ''
     });
+    const [error, setError] = useState(null);
 
     const handleChange = (e) => {
-
+        console.log('debug: ', e.target.value);
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
     }
 
-    const handleLogin = (e) => {
-
-    }
+    const handleLogin = async (e) => {
+        console.log('I am click the login button');
+        e.preventDefault();
+        try {
+            const response = await axios.post('https://node-jwt-e444b8b27b20.herokuapp.com/api/auth/login', credentials); // Replace with your API URL
+            console.log('Login successful:', response.data);
+            // Handle successful login (e.g., store token, redirect)
+        } catch (error) {
+            console.error('Login error:', error);
+            setError('Invalid username or password');
+        }
+    };
 
     return (
         <div>
+            <p> This is Login Page </p>
             <form onSubmit={handleLogin}>
                 <input
                     type="text"
@@ -33,7 +51,6 @@ export const Login = () => {
                 />
                 <button type="submit">Log in</button>
             </form>
-
         </div>
     )
 }
