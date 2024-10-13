a3d91777cc1b2aec4520a0c47440f6e3f04e130e
mingjun.geng <mingjun.geng@gmail.com>
2024-10-13 16:16:23 -0700

    add components - Home

---
+++ src/components/pages/Home.js
@@ -0,0 +1,9 @@
+import "../../../styles/Home.less"
+
+export const Home = () => {
+    return (
+        <div>
+            <p> This is a Home page. </p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+
+import { Home } from "./components/pages/Home";
 
 import "../styles/index.less"
 
 const container = document.getElementById("app");
 const root = createRoot(container)
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 function App() {
     return (
         <div >
-            <h1> I am h1 tag </h1>
-            <p> This is App Component</p>
-
+            <h1> hi this is index.js</h1>
+            <Routes>
+                <Route path="/home" element={<Home  />} />
+            </Routes>
         </div>
     )
 }
